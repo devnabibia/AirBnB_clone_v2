a692d07b1afeae50f6ca974a25ac4de41ef572ff
devnabibia <nangendodennis@gmail.com>
2023-10-19 15:18:12 +0000

    dev

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
-""" script that starts a Flask web application:
-    Your web application must be listening on 0.0.0.0, port 5000
-    Routes: 1- /: display “Hello HBNB!”
-            2- /hbnb: display “HBNB”
-            3- /c/<text>: display “C ” followed by the value of the text
-                variable (replace underscore _ symbols with a space )
-    You must use the option strict_slashes=False in your route definition """
+"""
+script that starts a Flask web application:
+Routes:
+* /: display “Hello HBNB!”
+* /hbnb: display “HBNB”
+* /c/<text>: display “C ” followed by the value of the text variable
+  (replace underscore _ symbols with a space )
+"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ hello_hbnb method """
-    return ('Hello HBNB!')
+def hello_HBNB():
+    """
+    Print a string
+    """
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def only_hbnb():
-    """ only_hbnb method """
-    return ('HBNB')
+@app.route('/hbnb/', strict_slashes=False)
+def display_HBNB():
+    """
+    Print a string
+    """
+    return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def only_c(text):
-    """ only_c method: route to return C followed by text variable, replaces _
-        with spaces """
-    text = text.replace('_', ' ')
-    return ('C' + ' ' + text)
+@app.route('/c/<text>/', strict_slashes=False)
+def C_is_fun(text):
+    """
+    Print a string
+    """
+    return 'C {}'.format(text.replace('_', ' '))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-""" this module contains a script that starts a Flask web application
-    the web application must be listening on 0.0.0.0, port 5000
-    Routes: - /states_list """
-from models import *
-from models.base_model import BaseModel, Base
-from models.user import User
-from models.place import Place
+"""
+script that starts a Flask web application.
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
 from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-from flask import Flask, render_template
+
 app = Flask(__name__)
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ display HTML page with list of states """
-    states = storage.all(classes["State"]).values()
-    # ^ fetches States data from storage engine, then in line below,
-    # those states are passed into the template
-    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def remove_SQLalc_session(exception):
-    """ close storage when tear down is executed """
+def teardown_db(exception):
+    """
+      Function to remove SQLAlchemy Session
+    """
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+@app.route('/states_list/', strict_slashes=False)
+def states_list():
+    """
+    Template html
+    """
+    the_states = storage.all(State).values()
+    """
+    my_list = []
+    for key, val in my_storage.items():
+        my_list.append(the_states[key])
+    """
+    return render_template('7-states_list.html', my_states=the_states)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
